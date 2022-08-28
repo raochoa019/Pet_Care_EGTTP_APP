3e4dc0e8e413ba655e6acae8ec689720754ed9a2
Robert Ochoa <raochoa@espol.edu.ec>
2022-08-28 02:53:01 -0500

    Cambios realizado 28-08-2022
    - Interfaz Menu con opciones
    - Seccion de comentarios (interfaz)
    - Consulta Api

--- PRY_LENG_PROG/PRY_LENG_PROG/PRY_LENG_PROG/SugerenciasComentarios/Comentarios.xaml.cs
+++ PRY_LENG_PROG/PRY_LENG_PROG/PRY_LENG_PROG/SugerenciasComentarios/Comentarios.xaml.cs
@@ -3,18 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using RestSharp;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using PRY_LENG_PROG.SugerenciasComentarios.ModelosComentario;
 
 namespace PRY_LENG_PROG.SugerenciasComentarios
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Comentarios : ContentPage
     {
+        List<DatosConsultaComentarios> ListComentarios = new List<DatosConsultaComentarios>();
         public Comentarios()
         {
             InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+            MessagingCenter.Send<Object>(this, "HideOsNavigationBar");
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            GetComments();
+        }
+
+        private void GetComments()
+        {
+            var Userclient = new RestClient("http://10.0.2.2:8000");
+            string ruta = "/api/comments";
+            var request = new RestRequest(ruta, Method.GET);
+            var queryResult = Userclient.Execute(request);
+            string strJason = queryResult.Content;
+            ListComentarios = JsonConvert.DeserializeObject<List<DatosConsultaComentarios>>(strJason);
+
+
+            
+            
+        } 
+
+
+        private void regresar_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void siguiente_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("msg", rating.Value.ToString(), "ok");            
         }
     }
 }
