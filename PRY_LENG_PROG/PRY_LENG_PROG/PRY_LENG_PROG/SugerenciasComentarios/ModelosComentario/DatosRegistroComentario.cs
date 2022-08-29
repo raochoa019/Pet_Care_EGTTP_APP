8a92141067f730240bf18dec5381323f26642f58
Robert Ochoa <raochoa@espol.edu.ec>
2022-08-28 23:25:34 -0500

    Cambios Realizados 28-08-2022
    - Actualizacion UserModel
    - Creacion Clase para registro de Comentarios
    - Creacion Interfaz para agregar usuarios
    - Funcionalidades implementadas para registro de comentarios
    - Actualizacion interfaz comentarios
    - Implementacion para validar usuarios

---
+++ PRY_LENG_PROG/PRY_LENG_PROG/PRY_LENG_PROG/SugerenciasComentarios/ModelosComentario/DatosRegistroComentario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRY_LENG_PROG.SugerenciasComentarios.ModelosComentario
+{
+    internal class DatosRegistroComentario
+    {
+        public int id_user { get; set; }
+        public string name_user { get; set; }
+        public int rating { get; set; }
+        public string title { get; set; }
+        public string comment { get; set; }
+    }
+}
