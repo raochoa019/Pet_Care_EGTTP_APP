807de17a2caf0ec5521676c4fad39421e46f6b23
Robert Ochoa <raochoa@espol.edu.ec>
2022-08-29 19:14:58 -0500

    Cambios 29-08-2022
    - Vista Hoteles

---
+++ PRY_LENG_PROG/PRY_LENG_PROG/PRY_LENG_PROG/Modelos/HotelModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PRY_LENG_PROG.Modelos
+{
+    public class HotelModel
+    {
+        public string nombreHotel { get; set; }
+        public string direccion { get; set; }
+    }
+}
---
+++ PRY_LENG_PROG/PRY_LENG_PROG/PRY_LENG_PROG/ReservaEstablecimiento/Hoteles.xaml.cs
@@ -0,0 +1,42 @@
+﻿using PRY_LENG_PROG.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PRY_LENG_PROG
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Hoteles : ContentPage
+    {
+        List<HotelModel> hoteles = new List<HotelModel>();
+        public Hoteles()
+        {
+            InitializeComponent();
+            hoteles.Add(new HotelModel {nombreHotel = "Hotel de las Flores", direccion = "Av. El Rey 333 y Mul Mul" });
+            hoteles.Add(new HotelModel { nombreHotel = "Hotel Oro Blue Hotel Resort", direccion = "Espejo y Av. Cevallos" });
+            hoteles.Add(new HotelModel { nombreHotel = "Hostal Grand Hotel", direccion = "Rocafuerte y Lalama"});
+            hoteles.Add(new HotelModel { nombreHotel = "Hotel Sangay", direccion = "	laza Isidro Ayora 100" });
+            hoteles.Add(new HotelModel { nombreHotel = "Hotel Oro Verde", direccion = "Ordóñez Laso s/n" });
+            hoteles.Add(new HotelModel { nombreHotel = "Hotel Las Américas", direccion = "Mariano Cueva 1359" });
+            hoteles.Add(new HotelModel { nombreHotel = "Hotel Grand Hotel Guayaquil", direccion = "Boyacá y 10 de Agosto" });
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            listView.ItemsSource = hoteles;
+        }
+
+        private void listView_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        {
+            HotelModel hotelSeleccionado = (HotelModel)e.ItemData;
+            //DisplayAlert("msg", hotelSeleccionado.nombreHotel, "ok");
+        }
+    }
+}
